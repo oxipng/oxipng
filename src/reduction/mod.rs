578e6730fc5223d6f130d36e7a58c1ffe8a5953a
Andrew <andrew@digerati.co.nz>
2026-06-21 14:34:20 +1200

    Rebalance levels

--- src/reduction/mod.rs
+++ src/reduction/mod.rs
@@ -21,11 +21,17 @@ pub(crate) fn perform_reductions(
 ) -> Arc<PngImage> {
     let mut evaluation_added = false;
 
-    // At low compression levels, skip some transformations which are less likely to be effective
-    // This currently affects optimization presets 0-2
-    let cheap = match opts.deflater {
-        Deflater::Libdeflater { compression } => compression < 12 && opts.fast_evaluation,
-        _ => false,
+    // Try to infer the optimization level from the settings
+    // TODO: [v11] Store the level in the settings
+    let effort = match opts.deflater {
+        Deflater::Libdeflater { compression } => match compression {
+            0..=9 => 0,
+            10 => 1,
+            11 if opts.filters.len() <= 4 => 2,
+            11 => 3,
+            _ => 4,
+        },
+        _ => 4,
     };
 
     // If alpha optimization is enabled, clean the alpha channel before continuing
@@ -110,7 +116,7 @@ pub(crate) fn perform_reductions(
 
     // Attempt to convert from indexed to channels
     // This may give a better result due to dropping the PLTE chunk
-    if !cheap
+    if effort >= 3
         && opts.color_type_reduction
         && !deadline.passed()
         && let Some(reduced) =
@@ -144,7 +150,7 @@ pub(crate) fn perform_reductions(
     };
 
     // Attempt to sort the palette using the ezeng method
-    if !cheap && opts.palette_reduction && !deadline.passed() {
+    if effort >= 3 && opts.palette_reduction && !deadline.passed() {
         // Make sure we use the `indexed` var as input if it exists
         let input = indexed.as_ref().unwrap_or(&png);
         if let Some(matrix) = CoOccurrenceMatrix::from(input) {
@@ -164,9 +170,9 @@ pub(crate) fn perform_reductions(
     if opts.bit_depth_reduction && !deadline.passed() {
         // First try the `png` var
         let reduced = reduced_bit_depth_8_or_less(&png);
-        // Then try the `indexed` var, unless we're doing cheap evaluations and already have a reduction
+        // Then try the `indexed` var, unless we're doing low-effort evaluations and already have a reduction
         // Only evaluate this if it's different from the first result (which must be grayscale if it exists)
-        if (!cheap || reduced.is_none())
+        if (effort >= 3 || reduced.is_none())
             && !deadline.passed()
             && let Some(indexed) = indexed.and_then(|png| reduced_bit_depth_8_or_less(&png))
             && reduced.as_ref().is_none_or(|r| r.data != indexed.data)
