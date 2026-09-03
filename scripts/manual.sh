5f105f2f641e6df239b7e55416c34f1feeb04397
Nuno Teixeira <eduardo@FreeBSD.org>
2026-09-03 01:20:44 +0100

    Enhance manual.sh to copy oxipng manpage
    Include manpage copy in the manual generation process.
    
    manpage is created with `cargo xtask mangen` in line 3 and no extra resourses are needed, just a plain copy to include it.
    
    As a oxipng FreeBSD port maintainer, our port just need to pick manpage from source tarball and install it instead of compile xtask to generate it.

--- scripts/manual.sh
+++ scripts/manual.sh
@@ -5,3 +5,5 @@ cargo xtask mangen
 ./target/debug/oxipng -V > MANUAL.txt
 #Redirect all streams to prevent detection of the terminal width and force an internal default of 100
 ./target/debug/oxipng --help >> MANUAL.txt 2>/dev/null </dev/null
+
+cp target/xtask/mangen/manpages/oxipng.1 oxipng.1
